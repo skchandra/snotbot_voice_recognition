cfa2492507e35b7b86577cbb972d894c3a22b744
Kiki <shivali.chandra@students.olin.edu>
2015-10-28 17:17:37 -0400

    Initial test

---
+++ initial_test.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+
+import speech_recognition as sr
+
+# obtain audio from the microphone
+r = sr.Recognizer()
+with sr.Microphone() as source:
+    print("Say something!")
+    audio = r.listen(source)
